df5cd3fb59e91c65a40df9ab8c07b8ffb4790195
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-02-18 00:49:52 +0000

    basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,14 @@
+// Created by: Adwok Adiebo
+// Created on: Feb 17, 2025
+// This program does basic math
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" << 9+2 << std::endl;
+    std::cout << "9-5=" <<  9-5 << std::endl;
+    std::cout << "5*6=" <<  5*6 << std::endl;
+    std::cout << "49/7=" << 49/7 << std::endl;
+    std::cout << "pow(10,2)=" << pow(10, 2)<< std::endl;
+    std::cout << "sqrt(16.0)=" << sqrt(16.0)<< std::endl;
+}
